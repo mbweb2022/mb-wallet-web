50b69f3456e14a53e90b7ab9ba1984b6e4418ffb
Jeremy21032 <jeremisma2001@hotmail.com>
2023-05-21 18:06:18 -0500

    JLE: Se modifica esquema de validaciones y presentación

--- src/sections/Contact/Contact.js
+++ src/sections/Contact/Contact.js
@@ -8,35 +8,80 @@ import { NAVBAR_COLOR } from '../../config';
 import FloatingButton from '../../pages/Terms/TermsPage'
 import { GREY_COLOR } from '../../config';
 import axios from 'axios';
-
+import { validateComment, validateEmail, validateUser } from '../../validations/validateRegex';
 const Contact = () => {
-
+  
     const [alert, setAlert] = useState(false);
     const [alertContent, setAlertContent] = useState('');
-
+    const [isValid, setIsValid]=useState(true);
     const formData = {user: "", email: "", detail: ""}
-    const [responseBody, setResponseBody] = useState(formData)
+    const [responseBody, setResponseBody] = useState(formData);
+    const [isValidEmail, setIsValidEmail] = useState(false);
+    const [isValidUser, setIsValidUser] = useState(false);
+    const [isValidComment, setIsValidComment] = useState(false);
+    const [hasInteracted, setHasInteracted] = useState(false);
+    const [errorTextEmail, setErrorTextEmail] = useState('');
+    const [errorTextUser, setErrorTextUser] = useState('');
+    const [errorTextComment, setErrorTextComment] = useState('');
+    const [isLoading, setLoading] = React.useState(false);
 
     const inputChangeHandler = (event) => {
-        const {name, value} = event.target
+        
+        const {name, value} = event.target;
+        handleValidateData(name, value);
+        
         setResponseBody({...responseBody, [name]: value})
     }
 
-    const handleSend = (event) => {
-        event.preventDefault();
 
-        axios.post('https://api.moneyblinks.com/contactus', responseBody)
+    const handleValidateData = (name, value)=>{
+        
+        if(name==="user"){
+            let response= validateUser(value);
+            setIsValidUser(response.Result);
+            setHasInteracted(true);
+            setErrorTextUser(response.message);
+        }
+        if(name==="email"){
+            let response= validateEmail(value);
+            setIsValidEmail(response.Result);
+            setHasInteracted(true);
+            setErrorTextEmail(response.message);
+        }
+        if(name==="detail"){
+            let response= validateComment(value);
+            setIsValidComment(response.Result);
+            setHasInteracted(true);
+            setErrorTextComment(response.message);
+        }
+        if(isValidUser&&isValidComment&&isValidEmail){
+            setIsValid(false);
+        }
+    }
+
+    
+    const handleSend = (event) => {
+        setLoading(true);
+        event.preventDefault();
+        axios.post('https://zwu92jsgu1.execute-api.us-east-1.amazonaws.com/web/contactus', responseBody)
             .then((resp) => {
                 if(resp.status === 200){
                     setAlertContent('ENVIADO CON EXITO');
                 } else {
                     setAlertContent('OCURRIO UN ERROR ENVIANDO EL MAIL');
-                }
+                }                
+                document.getElementById("user").value="";
+                document.getElementById("email").value="";
+                document.getElementById("detail").value="";
                 setAlert(true);
+                setLoading(false);
+
             })
             .catch((error) => {
-                console.log(error)
+                setAlert(true);
+                console.log("Ocurrio un error:" ,error)
                 setAlertContent('OCURRIO UN ERROR ENVIANDO EL MAIL');
+                setLoading(false);
             })
     }
 
@@ -62,6 +107,7 @@ const Contact = () => {
                 style={{ marginLeft: 50 }}
             >
                 <input
+                id="user"
                     type="text"
                     className='contact__input'
                     style={{
@@ -70,18 +116,28 @@ const Contact = () => {
                     name='user'
                     onChange={(e)=>inputChangeHandler(e)}
                     placeholder="Nombre"
+                    required
+                    disabled={isLoading}
                 />
+                    {!isValidUser&&errorTextUser && hasInteracted && <p className="error-message">{errorTextUser}</p>}
+
                 <input
-                    type="text"
+                    id="email"
+                    type="email"
                     className='contact__input'
                     name='email'
                     onChange={(e)=>inputChangeHandler(e)}
                     style={{
-                        height: "40px",
+                        height: "40px"
                     }}
                     placeholder="Mail"
+                    required
+                    disabled={isLoading}
+
                 />
+                    {!isValidEmail&&errorTextEmail && hasInteracted && <p className="error-message">{errorTextEmail}</p>}
                 <textarea
+                id="detail"
                     type="text"
                     className='contact__input'
                     name='detail'
@@ -93,9 +149,22 @@ const Contact = () => {
                         width: '140%',
                         alignSelf: 'flex-start'
                     }}
-                    placeholder="Consulta"
-                />
-                <button className='sendbutton' onClick={handleSend}>ENVIAR</button>
+                    placeholder="Motivo de consulta"
+                    disabled={isLoading}
+
+                    />
+                    {!isValidComment&&errorTextComment && hasInteracted && <p className="error-message">{errorTextComment}</p>}
+                <div className='row-button'>
+                <div class="column"><button className='sendbutton'style={{backgroundColor:isValid?"#878A8E":'rgb(206, 219, 30)',color:isValid?GREY_COLOR:'white'}} onClick={handleSend} disabled={isValid}>ENVIAR</button></div>
+                <div class="column">
+                {isLoading ? <div className="loadingdiv">
+                        <img src={'/assets/svg/cargando.svg'}width={234} alt="loading"/>
+                        <span style={{ fontSize: 18, fontFamily: "sans-serif" }}>Cargando, espera un momento...</span>
+                    </div> : null}    
+                </div>
+                    
+                   
+                </div>           
                 
                 {alert ? 
                     <div style={{ 
@@ -108,9 +177,10 @@ const Contact = () => {
                         color: GREY_COLOR,
                         fontWeight: '500'
                     }} className='alert-div' ><p>{alertContent}</p></div> : <></> 
-                }
+                } 
             </div>
-            
+
+
             <div className='contact_row'>
                 <Manito  style={{ width: 50, height: 30,  marginLeft: 20 }} />
                 <p className='contact__title' >CHATEA CON NOSOTROS</p>
@@ -121,6 +191,8 @@ const Contact = () => {
 
     </Dashed>
   )
+  
 }
+
 
 export default Contact;
---
+++ src/validations/validateRegex.js
@@ -0,0 +1,100 @@
+export const validateEmail = (ValueEmail) => {
+    let Response = {
+      Result: null,
+      message: null,
+      resultValidation: false,
+    };
+  
+    let expresion =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (expresion.test(ValueEmail)) {
+      let TempResult = validateEmailDomains(ValueEmail);
+      return TempResult;
+    } else {
+      Response.Result = false;
+      Response.resultValidation = true;
+      Response.message = "Correo electrónico invalido";
+      return Response;
+    }
+  };
+  
+  const validateEmailDomains = (ValueEmail) => {
+    let Response = {
+      Result: null,
+      message: null,
+      resultValidation: false,
+    };
+    let EmailSplit = ValueEmail.split("@");
+    EmailSplit = EmailSplit[1];
+    let domainsArray = [];
+    if (EmailSplit.includes(".")) {
+      domainsArray = EmailSplit.split(".");
+    }
+    const tempArrayDuplicate = [];
+    for (let i = 0; i < domainsArray.length; i++) {
+      if (domainsArray[i + 1] === domainsArray[i]) {
+        tempArrayDuplicate.push(domainsArray[i]);
+      }
+    }
+    if (tempArrayDuplicate.length === 0 &&domainsArray.length<=4) {
+      if (ValueEmail.endsWith(".")) {
+        Response.Result = false;
+        Response.resultValidation = true;
+        Response.message = "Correo electrónico invalido";
+        return Response;
+      } else {
+  
+        Response.Result = true;
+        Response.resultValidation = true;
+        Response.message = "Email Correcto";
+        return Response;
+      }
+    } else {
+      Response.Result = false;
+      Response.resultValidation = true;
+      Response.message = "Correo electrónico invalido";
+      return Response;
+    }
+  };
+  
+
+export const validateComment =(ValueComment)=>{
+    let Response = {
+        Result: null,
+        message: null,
+        resultValidation: false,
+      };
+    let expression = /^(?!\s*$).{10,}$/ ;
+    if (expression.test(ValueComment)) {
+        Response.Result = true;
+        Response.resultValidation = true;
+        Response.message = "Comentario Correcto";
+        return Response;
+      } else {
+        Response.Result = false;
+        Response.resultValidation = true;
+        Response.message = "El comentario debe tener al menos 10 caracteres y no puede estar en blanco.";
+        return Response;
+      }
+
+}
+export const validateUser =(ValueComment)=>{
+    let Response = {
+        Result: null,
+        message: null,
+        resultValidation: false,
+      };
+    let expression =/^([a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+)\s+([a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+)$/;
+    if (expression.test(ValueComment)) {
+        Response.Result = true;
+        Response.resultValidation = true;
+        Response.message = "Nombre de usuario Correcto";
+        return Response;
+      } else {
+        Response.Result = false;
+        Response.resultValidation = true;
+        Response.message = "Verifique que el Nombre de usuario esté correctamente escrito.";
+        return Response;
+      }
+
+}
