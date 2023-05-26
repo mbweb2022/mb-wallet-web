e5a2c95325fb72bb628e759a98b7d8261ee6a05b
JuanSlaterT <arevalobernaljuan@gmail.com>
2023-05-26 14:14:17 -0500

    fix tracking id

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import ScrollToTop from './helpers/ScrollToTop';
 import ReactGA from "react-ga";
-
+import { TRACKING_ID } from './config';
 ReactGA.initialize(TRACKING_ID);
 ReactGA.pageview(window.location.pathname + window.location.search);
 
