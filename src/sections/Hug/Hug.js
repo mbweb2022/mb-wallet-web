61684f0cce6b4f2adf6aefcfabbf5f3f084d0bf0
Jeremy21032 <jeremisma2001@hotmail.com>
2023-05-28 17:56:59 -0500

    JLE: Se agrega video

--- src/sections/Hug/Hug.js
+++ src/sections/Hug/Hug.js
@@ -1,33 +1,16 @@
 
 
-import React, { useState } from 'react'
+import React from 'react'
 import { Dashed } from '../../globalStyles'
 import './HugStyle.css';
 import { DefaultPlayer as Video } from 'react-html5video';
 import "react-html5video/dist/styles.css"
-import axios from "axios";
 export const Hug = () => {
-  const [urlVideo, setUrlVideo] = useState();
-  const load = async () => {
-    await axios.get("https://pastebin.com/gEs3irLb", {headers: {
-        'Cache-Control': 'no-cache',
-        'Pragma': 'no-cache',
-        'Expires': '0',
-    }})
-        .then((response) => {
-            const text = response.data;
-            console.log(text);
-            setUrlVideo(text)
-          
-        })
-        .catch(error => console.log(error));
-    
-}
-load();
+  
   return (
     <Dashed className='hug__section'>
       <Video autoPlay loop className="videoHug">
-        <source src={urlVideo} type='video/webm'></source>
+        <source src='https://moneyblinkswallets3125742-pre.s3.amazonaws.com/VIDEO+INSTITUCIONAL+5.mp4' type='video/webm'></source>
       </Video>
     </Dashed>
   )
