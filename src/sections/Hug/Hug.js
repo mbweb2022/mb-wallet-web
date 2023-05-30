75571c03ea1170e01f6c078889a843394288a4c6
Jeremy21032 <jeremisma2001@hotmail.com>
2023-05-29 21:50:19 -0500

    JLE: Se modifica implementación de video

--- src/sections/Hug/Hug.js
+++ src/sections/Hug/Hug.js
@@ -3,15 +3,22 @@
 import React from 'react'
 import { Dashed } from '../../globalStyles'
 import './HugStyle.css';
-import { DefaultPlayer as Video } from 'react-html5video';
+//import { DefaultPlayer as Video } from 'react-html5video';
+import ReactPlayer from 'react-player'
+
 import "react-html5video/dist/styles.css"
 export const Hug = () => {
   
   return (
     <Dashed className='hug__section'>
-      <Video autoPlay loop className="videoHug" poster="/assets/mbPoster.png">
+      {/* <Video autoPlay loop className="videoHug" poster="/assets/mbPoster.png">
         <source src='https://d3r4ipxr51o1re.cloudfront.net/BRAND_VIDEO.mp4' type='video/webm'></source>
-      </Video>
+      </Video> */}
+      {/* <ReactPlayer url="https://d3r4ipxr51o1re.cloudfront.net/BRAND_VIDEO.mp4"/> */}
+      <video  className="videoHug" autoPlay loop poster="/assets/mbPoster.png" controls>
+      <source src='https://d3r4ipxr51o1re.cloudfront.net/BRAND_VIDEO.mp4' type='video/webm'></source>
+
+      </video>
     </Dashed>
   )
 }
